36dcc1306f20a2ee7075b78ee9a2eedeeb427eb6
Projeto-Meteoro <guilherme.francisco@usp.br>
2023-05-28 11:39:21 +0100

    small change

--- src/components/Layout/Navbar/styles.ts
+++ src/components/Layout/Navbar/styles.ts
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-import NameLogo from '../../../assets/images/name-logo-light.png'
+import NameLogo from "../../../assets/images/name-logo-light2.png";
 
 export const Container = styled.div`
   display: flex;
@@ -9,14 +9,14 @@ export const Container = styled.div`
   /* height: 100%; */
   width: 220px;
   min-width: 150px;
-  background-color: ${({theme})=>theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
 
 export const Content = styled.div`
   width: inherit;
   position: fixed;
 
-  height:100vh;
+  height: 100vh;
 `;
 
 export const Logo = styled.div`
@@ -33,69 +33,69 @@ export const Logo = styled.div`
 `;
 
 export const Tab = styled(Link)`
-display: flex;
+  display: flex;
 
-text-decoration: none;
-color: ${({theme})=>theme.colors.light};
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.light};
 
-padding: 14px 20px;
-position:relative;
-
->div{
-  display:flex;
-  height: 100%;
-  width: 100%;
-
-  position: absolute;
-  top:0;
-  left:0;
-
-  border-left: 2px solid white;
-}
-
-p{
+  padding: 14px 20px;
   position: relative;
-  font-size: 14px;
-}
 
-img{
-  width: 20px;
-  height: 20px;
-  margin-right: 10px;
-  position: relative;
-}
+  > div {
+    display: flex;
+    height: 100%;
+    width: 100%;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    border-left: 2px solid white;
+  }
+
+  p {
+    position: relative;
+    font-size: 14px;
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    position: relative;
+  }
 `;
 export const ToggleContainer = styled.button`
-/* width:100% ; */
-display: flex;
-color: ${({theme})=>theme.colors.light};
-background-color:transparent ;
-border: none;
+  /* width:100% ; */
+  display: flex;
+  color: ${({ theme }) => theme.colors.light};
+  background-color: transparent;
+  border: none;
 
-padding: 14px 20px;
-position:relative;
-
->div{
-  display:flex;
-  height: 100%;
-  align-items: center;
-
-  position: absolute;
-  right: -10px;
-  top: 0;
-}
-
-p{
+  padding: 14px 20px;
   position: relative;
-  font-size: 14px;
-}
 
-img{
-  width: 20px;
-  height: 20px;
-  margin-right: 10px;
-  position: relative;
-}
+  > div {
+    display: flex;
+    height: 100%;
+    align-items: center;
+
+    position: absolute;
+    right: -10px;
+    top: 0;
+  }
+
+  p {
+    position: relative;
+    font-size: 14px;
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    position: relative;
+  }
 `;
 
 export const ActionButonsContainer = styled.div`
@@ -115,14 +115,14 @@ export const ActionButton = styled.button`
   /* position:absolute;
   bottom: 0; */
 
-  background-color:transparent ;
-  text-decoration:none ;
-  border:none ;
+  background-color: transparent;
+  text-decoration: none;
+  border: none;
 
   gap: 5px;
 
-  color: ${({theme})=>theme.colors.light};
-  font-size: 14px;;
+  color: ${({ theme }) => theme.colors.light};
+  font-size: 14px;
 
   margin: 20px 10px;
 `;
